19e930556a966240d3f4a761291958624e71d272
Aishwaryaacharya-2004 <aishwaryaacharyapk2004@gmail.com>
2025-05-25 04:36:16 +0530

    new schema

---
+++ models/Feedback.js
@@ -0,0 +1,18 @@
+// models/Feedback.js
+const mongoose = require('mongoose');
+
+const FeedbackSchema = new mongoose.Schema({
+    name:{type:String, required:true},
+    email: { type: String, required: true },
+    event: { type: String, required: true },
+    answers: {
+        q1: String,
+        q2: String
+    },
+    submittedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Feedback', FeedbackSchema);
